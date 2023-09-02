eaca53e39ba1e2819cca7f8470581c9dadf0f5ab
PNT1AGrupo3 <111472309+PNT1AGrupo3@users.noreply.github.com>
2023-09-01 23:24:50 -0300

    primera version pokemons/lectura de datos de una bd a vs

---
+++ TP-INTEGRADOR/Pokemon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_INTEGRADOR
+{
+    class Pokemon
+    {
+        public int Numero { get; set; }
+        public String Nombre { get; set; }
+
+        public String Descripcion { get; set; }
+    }
+}
---
+++ TP-INTEGRADOR/PokemonNegocio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+
+namespace TP_INTEGRADOR
+{
+    class PokemonNegocio
+    {
+        public List<Pokemon> listar() {
+
+            List<Pokemon> lista = new List<Pokemon>();
+            SqlConnection
+        }
+
+    }
+}
